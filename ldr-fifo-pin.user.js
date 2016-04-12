126552e9715609ca65831708db062191a744ddde
iwamot <hello@iwamot.com>
2016-04-12 23:25:23 +0900

    新ドメインに対応

--- ldr-fifo-pin.user.js
+++ ldr-fifo-pin.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         LDR - FIFO Pin
 // @namespace    http://iwamot.com/
-// @version      1.0.2
+// @version      1.0.3
 // @author       IWAMOTO Takashi <hello@iwamot.com> http://iwamot.com/
 // @description  livedoor Readerのピンの挙動をLIFO（後入れ先出し）からFIFO（先入れ先出し）に変えます。
 // @include      http://reader.livedoor.com/reader/*
+// @include      http://reader.livedwango.com/reader/*
 // @homepage     https://github.com/iwamot/ldr-fifo-pin
 // @updateURL    https://github.com/iwamot/ldr-fifo-pin/raw/master/ldr-fifo-pin.user.js
 // ==/UserScript==
